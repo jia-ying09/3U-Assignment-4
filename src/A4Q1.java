9b970581f9a3c8cbd2a2e6f68a2d56e0fa344f54
linj4653 <linj4653@HRH-IC0027333.wrdsb.ca>
2017-03-06 10:12:48 -0500

    Assignment 4 first draft

---
+++ src/A4Q1.java
@@ -0,0 +1,30 @@
+
+import becker.robots.City;
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //creating a scanner to get user input
+        Scanner input = new Scanner (System.in);
+        //asking user to enter name
+        System.out.println("Please enter your name:");
+        
+        //scanning name into system input
+        String name = input.nextLine();
+
+        System.out.println("Hello " + name + "." + " How are you today?");
+    }
+}
---
+++ src/A4Q2.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A4Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // inputting scanner
+        Scanner input = new Scanner (System.in);
+        
+        //ask user for measurement
+        System.out.println("What is the measurement in inches?");
+        String number = input.nextLine();
+        int quotient = 6/2.54;
+        
+    }
+}
