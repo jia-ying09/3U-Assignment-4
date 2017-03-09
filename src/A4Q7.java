f4c88028fd25c79abb035f3f5635815650236d99
linj4653 <linj4653@HRH-IC0027333.wrdsb.ca>
2017-03-09 10:14:51 -0500

    Assignment 4 first draft

---
+++ src/A4Q7.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author linj4653
+ */
+public class A4Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // import scanner
+        Scanner input = new Scanner(System.in);
+        
+        //ask
+    }
+}
